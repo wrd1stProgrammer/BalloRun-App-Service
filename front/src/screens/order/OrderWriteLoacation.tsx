b0db91bf6e24fd434c6f81f0c2a5f24969a7bcf5
parkyoungseo <givpro22@daum.net>
2025-01-08 23:01:38 +0900

    25-01-08 MapAdd

--- front/src/screens/order/OrderWriteLoacation.tsx
+++ front/src/screens/order/OrderWriteLoacation.tsx
@@ -1,14 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-const OrderWriteLoacation: React.FC = () => {
-  return (
-    <View>
-      <Text>OrderWriteLoacation Screen</Text>
-    </View>
-  )
-}
 
-export default OrderWriteLoacation
-
-const styles = StyleSheet.create({})
+  const OrderWriteLoacation = () => {
+    return (
+      <View>
+        <MapView
+          provider={PROVIDER_GOOGLE} 
+          initialRegion={{
+            latitude: 37.541,
+            longitude: 126.986,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
+       </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    map: {
+      height: "100%",
+      width: "100%"
+    }
+  });
+  
+  export default OrderWriteLoacation;
