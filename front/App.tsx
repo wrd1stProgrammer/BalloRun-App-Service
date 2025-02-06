6b69dc90ebb52c8a0419d35f9b0d6f1b169077af
ParkYoungSeo <givpro22@daum.net>
2025-02-06 16:37:20 +0900

    25-02-06 ios-bar

--- front/App.tsx
+++ front/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Platform } from "react-native";
+import { StyleSheet, View, Platform } from "react-native";
 import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Provider } from "react-redux";
@@ -10,40 +10,42 @@ import WebSocketContainer from "./src/utils/sockets/Socket";
 import './reanimatedConfig';
 import ChatSocketContainer from "./src/utils/sockets/ChatSocket";
 import MapSocketContainer from "./src/utils/sockets/MapSocket";
-import { setupBackgroundNotifications,setupForegroundNotifications,onNotificationOpenedApp } from "./src/utils/fcm/FcmHandler";
-import { startFcm } from "./src/utils/fcm/fcmInitailizeApp";
+import { setupBackgroundNotifications, setupForegroundNotifications, onNotificationOpenedApp } from "./src/utils/fcm/FcmHandler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { getStatusBarHeight } from "react-native-status-bar-height"; // 상태 바 높이 가져오기
 
 const App: React.FC = () => {
-  //fcm client 포/백 그라운드 설정.
   useEffect(() => {
-
-    // startFcm(); -> AppDelegate.mm 에서 시작해 주석 처리
-
     const foregroundListener = setupForegroundNotifications();
     setupBackgroundNotifications();
     onNotificationOpenedApp();
     return () => {
-      foregroundListener(); // 언마운트 -> 리스너 정리 음 아마
+      foregroundListener(); // 리스너 정리
     };
   }, []);
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar
-        translucent={Platform.OS === "ios"}
-        backgroundColor="transparent"
-      />
-      <Provider store={store}>
-        <WebSocketContainer>
-          <MapSocketContainer>
-          <ChatSocketContainer>
-          <PersistGate loading={null} persistor={persistor}>
-            <Navigation />
-          </PersistGate>
-          </ChatSocketContainer>
-          </MapSocketContainer>
-        </WebSocketContainer>
-      </Provider>
+      {/* SafeAreaView에서 상단만 보호 */}
+      <SafeAreaView style={{ flex: 1, backgroundColor: "white" }} edges={["top"]}>
+        <StatusBar translucent backgroundColor="transparent" />
+        
+        {/* 기기별 상태 바 높이에 맞춰 자동 조정 */}
+        <View style={{ flex: 1, paddingTop: Platform.OS === "ios" ? getStatusBarHeight(true) : 0 }}>
+          <Provider store={store}>
+            <WebSocketContainer>
+              <MapSocketContainer>
+                <ChatSocketContainer>
+                  <PersistGate loading={null} persistor={persistor}>
+                    <Navigation />
+                  </PersistGate>
+                </ChatSocketContainer>
+              </MapSocketContainer>
+            </WebSocketContainer>
+          </Provider>
+        </View>
+        
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
 };
